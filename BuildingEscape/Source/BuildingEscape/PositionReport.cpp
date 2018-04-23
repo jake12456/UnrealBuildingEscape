f2c111c1962b7d568fac064c2a8d990a508ef545
DESKTOP-HK943QS\Jake Jeffery <jake12456@gmail.com>
2018-04-22 18:48:15 -0700

    Updating git ignore and adding name print out in console log.

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -9,6 +9,7 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	UE_LOG(LogTemp, Warning, TEXT("Position Report Reporting!\n"));
 
 	// ...
 }
@@ -18,7 +19,9 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
+	FString ownerName = GetOwner()->GetName();
+	
+	UE_LOG(LogTemp, Warning, TEXT("Position Report Begin Play : %s!\n"), *ownerName);
 	// ...
 	
 }
